8d341a78df60872e8708334db99a5a4a5342dc5b
kjpalof <katie.palof@alaska.gov>
2019-08-12 17:01:37 -0800

    libraries generic

---
+++ SMBKC/code/packages.R
@@ -0,0 +1,28 @@
+# packages to load generic
+# Katie Palof
+# katie.palof@alaska.gov
+# 2018-3-23, 2019-8-12
+
+# load -----
+library(tidyverse)
+library(xlsx)
+library(readxl)
+library(reshape2)
+library(scales)
+library(extrafont)
+library(grid)
+library(knitr)
+library(pander)
+library(data.table)
+library(readr)
+library(purrr)
+library(stringr)
+options(scipen=9999) # remove scientific notation
+
+loadfonts(device="win")
+windowsFonts(Times=windowsFont("TT Times New Roman"))
+theme_set(theme_bw(base_size=12,base_family='Times New Roman')+ 
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()))
+
+# functions ------------
