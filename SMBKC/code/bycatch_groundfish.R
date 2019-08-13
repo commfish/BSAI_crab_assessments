6bd7ec1e3fc018e076a76e45ecbfe6f00801d44b
kjpalof <katie.palof@alaska.gov>
2019-08-12 17:54:17 -0800

    code for bycatch by weight.  Check units here

--- SMBKC/code/bycatch_groundfish.R
+++ SMBKC/code/bycatch_groundfish.R
@@ -26,3 +26,12 @@ gf_bycatch %>%
   dcast( ., Crab.Year ~ Agency.Gear.Code, sum) %>% 
   mutate(trawl = (NPT + PTR)/2, fixed = (HAL + POT)/2) -> gf_by_nums# combine trawl and fixed, divide by 2 - MALE only
 # ignore 2019, this is just the beginning of this season, data up until 2018
+
+# weight -
+gf_bycatch %>% 
+  group_by(Crab.Year, Agency.Gear.Code) %>% 
+  summarise(sum = sum(Estimate.Wt..kg.crab.)) %>% 
+  dcast( ., Crab.Year ~ Agency.Gear.Code, sum) %>% 
+  mutate(trawl = (NPT + PTR)/2, fixed = (HAL + POT)/2, 
+         trawl_thou = trawl/1000, fixed_thou = fixed/1000) -> gf_by_weight# combine trawl and fixed, divide by 2 - MALE only
+# ignore 2019, this is just the beginning of this season, data up until 2018
