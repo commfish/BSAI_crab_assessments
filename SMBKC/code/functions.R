f9c4b1bcf3bd7b16e25dcc1831086fd3368ede4c
kjpalof <katie.palof@alaska.gov>
2019-08-26 13:13:02 -0800

    functions used for rebuiling projections and looking at mcmc output

---
+++ SMBKC/code/functions.R
@@ -0,0 +1,174 @@
+# k palof
+# 04-09-2019 
+# user created functions for smbkc projections
+
+f_sum <- function(data, col1, col2, div){
+  col1 = enquo(col1)
+  col2 = enquo(col2)
+  div = enquo(div)
+  
+  data %>% 
+    dplyr::select(!!col1:!!col2) %>% 
+    apply(1, sum, na.rm=T) %>% 
+    as.data.frame %>% 
+    bind_cols(data) %>% 
+    transmute(temp = . / !!div * 100) %>% 
+    .$temp
+}  
+
+
+f_test <- function(x){
+  as.numeric(x > 100)
+}
+
+
+# function to output .csv and .png ------------------------
+
+write_rec_prob <- function(n_prob_yr, model, version) {
+  TheD <- read.table(paste0(here::here(), "/SMBKC/smbkc_18a/projections/", model, "/", version, "/mcoutPROJ.rep"))[,-c(4,5,6,7,8)]
+  
+  Nyear <- length(TheD[1,])-4
+  Nline <- length(TheD[,1])
+  print(Nyear)
+  print(Nline)
+  
+  # prob of recovery 
+  for (Iline in 1:Nline)
+    TheD[Iline,5:(4 + Nyear)] <- TheD[Iline, 5:(4 + Nyear)] / TheD[Iline, 4] * 100 
+  
+  
+  TheD %>% 
+    #mutate(ratio = f_sum(., V10, V59, V9)) %>% 
+    mutate_at(vars(-V1, -V2, -V3, -V9), f_test) %>% 
+    mutate(id = 1:n()) %>% 
+    gather(year, value, -V1, -V2, -V3, -V9, -id) %>% 
+    group_by(id) %>% 
+    mutate(test = as.numeric(lead(value) + value == n_prob_yr),
+           year = as.numeric(as.factor(year))) %>%  #this doesn't work if you get above V99
+    group_by(year, V3) %>% 
+    summarise(recovery = sum(test, na.rm = TRUE) / n() * 100)  %>% 
+    mutate(FishMort = ifelse(V3 == 1, "F = 0", "F = 0.18")) -> output
+  #write_csv("test.csv")
+  write_csv(output, paste0('./projections/', model, '/', version, '/rec_prob_out_', model, version, '.csv'))
+  
+  year1 <- output[1:2, ]
+  year1 %>% 
+    mutate(year2 = year) -> year1
+  
+  output %>% 
+    mutate(year2 = year + 1) %>% 
+    bind_rows(year1) %>% 
+    filter(year2 <= Nyear) %>% 
+    ggplot(aes(year2, recovery, group = V3)) + 
+    geom_line() +
+    geom_point(aes(group = V3, shape = FishMort), size = 2) +
+    scale_shape_manual(name = "", values = c(16, 22)) +
+    geom_hline(yintercept = 50, color = "red", lty = "dashed", lwd = 1.5) +
+    geom_vline(xintercept = 10, color = "blue", lty = 2, lwd = 1.5) +
+    ggtitle(paste0(model, version)) +
+    ylab("Probability of recovery") +
+    xlab("Year") +
+    ylim(0,100) +
+    theme(plot.title = element_text(hjust = 0.5)) -> plotA
+  ggsave(paste0('./projections/figures/', model, '_', version, '_rec_prob.png'), plotA, dpi = 800,
+         width = 7.5, height = 3.75)
+  
+}
+
+# 1 year above Bmsy proxy -- function to output .csv and .png ------------------------
+
+write_rec_prob1 <- function(n_prob_yr, model, version) {
+  TheD <- read.table(paste0(here::here(), "/SMBKC/smbkc_18a/projections/", model, "/", version, "/mcoutPROJ.rep"))[,-c(4,5,6,7,8)]
+  
+  Nyear <- length(TheD[1,])-4
+  Nline <- length(TheD[,1])
+  print(Nyear)
+  print(Nline)
+  
+  # prob of recovery 
+  for (Iline in 1:Nline)
+    TheD[Iline,5:(4 + Nyear)] <- TheD[Iline, 5:(4 + Nyear)] / TheD[Iline, 4] * 100 
+  
+  TheD %>% 
+    #mutate(ratio = f_sum(., V10, V59, V9)) %>% 
+    mutate_at(vars(-V1, -V2, -V3, -V9), f_test) %>% 
+    mutate(id = 1:n()) %>% 
+    gather(year, value, -V1, -V2, -V3, -V9, -id) %>% 
+    group_by(id) %>% 
+    mutate(year = as.numeric(as.factor(year))) %>%  #this doesn't work if you get above V99
+    group_by(year, V3) %>% 
+    summarise(recovery = sum(value, na.rm = TRUE) / n() * 100)  %>% 
+    mutate(FishMort = ifelse(V3 == 1, "F = 0", "F = 0.18")) -> output
+  #write_csv("test.csv")
+  write_csv(output, paste0(here::here(), '/SMBKC/smbkc_18a/projections/', model, '/', version, '/rec_1yr_prob_out_', model, version, '.csv'))
+  
+  year1 <- output[1:2, ]
+  year1 %>% 
+    mutate(year2 = year) -> year1
+  
+  output %>% 
+    mutate(year2 = year + 1) %>% 
+    bind_rows(year1) %>% 
+    filter(year2 <= Nyear) %>% 
+    ggplot(aes(year2, recovery, group = V3)) + 
+    geom_line() +
+    geom_point(aes(group = V3, shape = FishMort), size = 2) +
+    scale_shape_manual(name = "", values = c(16, 22)) +
+    geom_hline(yintercept = 50, color = "red", lty = "dashed", lwd = 1.5) +
+    geom_vline(xintercept = 10, color = "blue", lty = 2, lwd = 1.5) +
+    ggtitle(paste0(model, version)) +
+    ylab("Probability of recovery") +
+    xlab("Year") +
+    ylim(0,100) +
+    theme(plot.title = element_text(hjust = 0.5)) -> plotA
+  ggsave(paste0(here::here(), '/SMBKC/smbkc_18a/projections/figures/', model, '_', version, '_rec_1yr_prob.png'), plotA, dpi = 800,
+         width = 7.5, height = 3.75)
+  
+}
+
+
+
+
+## uncertainty in just 2019 projection -------
+un_cur_yr("smbkc_19", "proj_1", "d", "model_1", "19.0 (ref)")
+#model_yr <- "smbkc_19"
+#proj <- "proj_1" # projection set up - involve recruitment years
+#version <- "d" # projection version - see readme
+#label <- "19.0 (ref)" # model label - used for SAFE
+#model <- "model_1" # model name in files
+
+un_cur_yr <- function(model_yr, proj, version, model, label) {
+  TheD <- read.table(paste0(here::here(), "/SMBKC/", model_yr ,"/", model,"/projections/", proj, "/", version, "/mcoutPROJ.rep"))[,-c(4,5,6,7,8)]
+  
+  Nyear <- length(TheD[1,])-4 #each year is its own column
+  Nline <- length(TheD[,1]) #1000 for each scenario direct F=0, F=0.18
+  print(Nyear)
+  print(Nline)
+  TheD %>% 
+    filter(V3 == 1) %>% 
+    select(V1, V2, V3, Bmsy = V9, curyr = V10) %>% 
+    summarise(lci = quantile(curyr, 0.05),
+              uci = quantile(curyr, 0.95), 
+              median_ssb = median(curyr)) %>% 
+    mutate(Model = label) -> uncertain_current
+  #write_csv(uncertain_current, paste0(here::here(), 
+   #                                   '/SMBKC/', model_yr,'/', model,'/projections/', proj, '/', 
+  #                                    version, '/uncertainty_ssb_', model, "_", proj, '_', version, '_', cur_yr, '.csv'))
+  write_csv(uncertain_current, paste0(here::here(), 
+                                  '/SMBKC/', model_yr,'/', model,'/projections/', proj, '/', 
+                                  version, '/uncertainty_ssb_', cur_yr, '.csv'))
+}
+
+
+
+## just year 1 (next year or projection year) and F =0
+TheD %>% 
+  filter(V3 == 1) %>% 
+  select(V1, V2, V3, Bmsy = V9, yr2019 = V10) %>% 
+  summarise(lci = quantile(yr2019, 0.05),
+            uci = quantile(yr2019, 0.95), 
+            median_ssb = median(yr2019)) %>% 
+  mutate(Model = "19.0") -> uncertain_current
+write_csv(uncertain_current, paste0(here::here(), 
+                                    '/SMBKC/smbkc_19/projections/', model, '/', 
+                                    version, '/uncertainty_ssb_', model, version, '_', cur_yr, '.csv'))
