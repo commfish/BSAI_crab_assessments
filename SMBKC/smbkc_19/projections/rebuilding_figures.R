06d5d182ce26decc3e22839d2c10816928955a8e
kjpalof <katie.palof@alaska.gov>
2019-08-30 10:54:34 -0800

    rebuilding figures

---
+++ SMBKC/smbkc_19/projections/rebuilding_figures.R
@@ -0,0 +1,15 @@
+# katie.palof@alaska.gov      8-30-19
+# Rebuilding projections for SMBKC figures
+
+# results from the projections can be found in the /projections/proj_x/x folders for each model type
+
+# here look at those from smbkc_19/model_1/projections
+
+# load -----------
+# load -------
+source("./SMBKC/code/helper.R")
+source("./SMBKC/code/functions.R") # load function for summarising output from projection
+
+# data ---------
+proj1d <- 
+proj1aa <- 
