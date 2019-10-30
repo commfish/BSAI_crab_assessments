4720b7ddc3ea9372cfb24292a7d8b861536151a4
kjpalof <katie.palof@alaska.gov>
2019-10-30 14:40:43 -0800

    update with projection 2b which is F=M or alternative 1 in rebuliding plan

--- SMBKC/smbkc_19/projections/rebuilding_figures.R
+++ SMBKC/smbkc_19/projections/rebuilding_figures.R
@@ -21,6 +21,7 @@ proj4d <- read.csv(here::here("SMBKC/smbkc_19/model_1/projections/proj_4/d/rec_1
 
 proj2d <- read.csv(here::here("SMBKC/smbkc_19/model_1/projections/proj_2/d/rec_1yr_prob_out_proj_2d.csv"))
 proj2aa <- read.csv(here::here("SMBKC/smbkc_19/model_1/projections/proj_2/aa/rec_1yr_prob_out_proj_2aa.csv"))
+proj2b <- read.csv(here::here("SMBKC/smbkc_19/model_1/projections/proj_2/b/rec_1yr_prob_out_proj_2b.csv"))
 
 ## projection 1 --------
 # the label for F =0.18 needs to be SHR or state harvest rate 
@@ -232,7 +233,7 @@ proj2 %>%
   geom_line() +
   geom_hline(yintercept = 50, color = "red", lty = "dashed", lwd = 1.5) +
   geom_vline(xintercept = 10, color = "blue", lty = 2, lwd = 1.5) +
-  ggtitle("Ricker stock-recruit relationship (Bmsy proxy 1978 - 2018)") +
+  ggtitle(expression(paste("Ricker stock-recruit relationship (", B[MSY]," proxy 1978 - 2018)"))) +
   ylab("Probability of recovery") +
   xlab("Year") +
   ylim(0,100) +
@@ -240,3 +241,42 @@ proj2 %>%
 ggsave(paste0(here::here(), '/SMBKC/smbkc_19/doc/safe_figure/proj2ALL_rec_1yr_prob.png'), plotA, dpi = 800,
        width = 7.5, height = 3.75)
 
+## proj 2 avg by, max by, and F=M, for rebuilding document -------------
+proj2d %>% 
+  mutate(projection = "avg recent bycatch") %>% 
+  select(-FishMort) %>% 
+  mutate(FishMort = ifelse(V3 == 1, "F = 0", "F = SHR")) -> proj2d
+
+proj2aa %>% 
+  mutate(projection = "max bycatch") %>% 
+  select(-FishMort) %>% 
+  mutate(FishMort = ifelse(V3 == 1, "F = 0", "F = SHR")) -> proj2aa
+
+proj2b %>% 
+  mutate(projection = "alternative 1") %>% 
+  filter(V3 == 2) %>% 
+  select(year, V3, recovery, projection, FishMort) -> proj2b
+  #elect(-FishMort) %>% 
+  #mutate(FishMort = ifelse(V3 == 1, "F = 0", "F = SHR")) -> proj2b
+
+proj2d %>% 
+  bind_rows(proj2aa) %>% 
+  bind_rows(proj2b) -> proj2
+
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+proj2 %>% 
+  ggplot(aes(year, recovery, shape = FishMort, colour = projection)) + 
+  geom_point(size = 2)+
+  scale_shape_manual(name = "", values = c(16, 17, 22)) +
+  scale_color_manual(name = "", values = cbPalette[1:3])+
+  geom_line() +
+  geom_hline(yintercept = 50, color = "red", lty = "dashed", lwd = 1.5) +
+  geom_vline(xintercept = 10, color = "blue", lty = 2, lwd = 1.5) +
+  ggtitle(expression(paste("Ricker stock-recruit relationship (", B[MSY]," proxy 1978 - 2018)"))) +
+  ylab("Probability of recovery") +
+  xlab("Year") +
+  ylim(0,100) +
+  theme(plot.title = element_text(hjust = 0.5)) -> plotA
+ggsave(paste0(here::here(), '/SMBKC/smbkc_19/doc/rebuilding_2019/proj2_rec_1yr_prob_REBUILD.png'), plotA, dpi = 800,
+       width = 7.5, height = 3.75)
