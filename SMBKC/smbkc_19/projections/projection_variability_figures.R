3170a0d2058e2034d2ad10f7f2dfdd849790d884
kjpalof <katie.palof@alaska.gov>
2020-02-13 16:00:15 -0900

    edits to the variably figure for projection 2. needs further work

--- SMBKC/smbkc_19/projections/projection_variability_figures.R
+++ SMBKC/smbkc_19/projections/projection_variability_figures.R
@@ -25,29 +25,35 @@ raw %>%
   #filter(V3 == 1) %>% 
   #mutate(id = 1:n()) %>% 
   gather(year, mmb, -V1, -V2, -V3, -V9) %>% 
-  mutate(year = as.numeric(as.factor(year))) -> raw_all 
+  mutate(year = as.numeric(as.factor(year)), 
+         FishMort = ifelse(V3 == 1, "F = 0", "F = SHS")) -> raw_all 
 
 raw_all %>% 
-  group_by(V3) %>% 
-  summarise(Bmsy = mean(V9)) -> Bmsy_all
+  summarise(Bmsy = mean(V9)) -> Bmsy
 
 # 95 % of the distribution in gray. Average MMB in year projection in solid line
 raw_all %>% 
-  group_by(year, V3) %>% 
+  group_by(year, FishMort) %>% 
   summarise(q0.05 = quantile(mmb, prob = 0.05), 
             q0.25 = quantile(mmb, prob = 0.25),
             q0.50 = quantile(mmb, prob = 0.50),
             q0.75 = quantile(mmb, prob = 0.75),
             q0.95 = quantile(mmb, prob = 0.95), 
             Bmsy = mean(V9)) %>% 
-  ggplot(aes(year, q0.50, group = V3)) +
+  ggplot(aes(year, q0.50, colour = FishMort)) +
   geom_line(lwd = 1) +
-  geom_ribbon(aes(ymin = q0.05, ymax = q0.95, x = year, fill = V3), alpha = 0.17)+
-  #geom_line(aes(year, q0.95), lwd = 0.5, color = "blue") +
-  #geom_line(aes(year, q0.05), lwd = 0.5, color = "blue") +
+  scale_color_manual(name = "", values = c(cbPalette[4], cbPalette[1]))+
+  geom_ribbon(aes(ymin = q0.05, ymax = q0.95, x = year, fill = FishMort), alpha = 0.17) +
+  scale_fill_manual(name = "", values = c(cbPalette[4], cbPalette[1]))+
   geom_hline(yintercept = Bmsy[1,], lwd = 0.75, color = "darkgoldenrod4", linetype = "dashed") +
-  ylab ("MMB (units)") +
-  xlab ("Projection year")
+  geom_text(aes(0, 3300, label = "BMSY proxy",
+                vjust = -1, hjust = 0.05)) +
+  ylab ("MMB (tons)") +
+  xlab ("Projection Year") +
+  ggtitle("Ricker S-R recruitment, average bycatch levels") #-> plotA
+
+ggsave(paste0(here::here(), '/SMBKC/smbkc_19/doc/rebuilding_2019/proj2d_variability.png'), plotA, dpi = 800,
+       width = 7.5, height = 3.75)
 
 # for Fishing Mortality F = 0, that's what v3 = 1 stands for -----------
 raw %>% 
