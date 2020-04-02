30b1ed8222a4e02e19f87cafc4294052744347de
kjpalof <katie.palof@alaska.gov>
2020-04-01 18:36:19 -0800

    code to visually examine the VAST estimates from Jon Richer (NMFS) April 2020

---
+++ SMBKC/code/vast_biomass_est_comparison.R
@@ -0,0 +1,39 @@
+# VAST input for model
+
+# katie palof, katie.palof@alaska.gov
+# VAST estimates produced by Jon Richar at NOAA Kodiak for SMBKC trawl survey only
+
+# VAST data --------
+data_vast_19 <- read.csv(here::here("SMBKC/smbkc_19a/data/SMBKC_GE90_BIOMASS.csv"))
+
+
+# current data in model -------------
+data_trawl <- read.csv(here::here("SMBKC/smbkc_19/data/survey_biomass_mt.csv"))
+
+
+### data clean up --------
+data_vast_19 %>% 
+  filter(Year >= 1978) -> data_vast_19
+data_trawl %>% 
+  filter(SURVEY_YEAR >= 1978) -> data_trawl
+
+# visualization -------
+data_vast_19 %>% 
+  filter(Year >= 1978) %>% 
+  ggplot(aes(Year, Estimate_metric_tons)) +
+  geom_line()
+
+
+data_trawl %>% 
+  filter(SURVEY_YEAR >= 1978) %>% 
+  ggplot(aes(SURVEY_YEAR, BIOMASS_MT)) +
+  geom_line()
+
+
+data_trawl %>% 
+  select(Year = SURVEY_YEAR, BIOMASS_MT, BIOMASS_MT_CV) %>% 
+  bind_cols(data_vast_19) %>% 
+  select(Year, BIOMASS_MT, BIOMASS_MT_CV, Estimate_metric_tons, SD_mt) %>% 
+  ggplot(aes(Year, BIOMASS_MT)) +
+  geom_line() +
+  geom_line(aes(Year, Estimate_metric_tons), col = "red")
