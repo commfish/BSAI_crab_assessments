0485191482abcd192b99595923134396778adb70
kjpalof <katie.palof@alaska.gov>
2020-08-16 10:24:30 -0800

    add in notes

--- SMBKC/smbkc_20/model_1/plot.R
+++ SMBKC/smbkc_20/model_1/plot.R
@@ -13,7 +13,8 @@
 #devtools::install_github("seacode/gmacs", subdir = "/gmr", ref = "develop") #, INSTALL_opts="--no-staged-install") #- only needs to be performed once, but needs to be 
 # done again when updates are made to the package
 # if gmr is updated and above doesn't work try:
-# go to Build above - "configure build tools" - direct it to the gmr folder - press OK. Over on right in the Build tab (upper right hand side) - 
+# go to Build above - "configure build tools" - direct it to the gmr folder - press OK. 
+# Over on right in the Build tab (upper right hand side) - 
 # click "install and restart"
 
 require(gmr)
