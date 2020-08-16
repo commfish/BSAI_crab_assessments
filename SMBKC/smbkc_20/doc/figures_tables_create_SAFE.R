1f16881ce07b5d533338f72d3162fa044d04397a
kjpalof <katie.palof@alaska.gov>
2020-08-16 10:48:20 -0800

    comparison between reference 2019 and 2020 base

--- SMBKC/smbkc_20/doc/figures_tables_create_SAFE.R
+++ SMBKC/smbkc_20/doc/figures_tables_create_SAFE.R
@@ -1,6 +1,6 @@
 # k.palof
-# 4-26-2020
-# Figures and Tables needed for may 2020 SAFE
+# 4-26-2020 / 8-16-2020
+# Figures and Tables needed for Sept 2020 SAFE SMBKC
 
 # Should be able to just run this file if model results are updated prior to creating
 #   new .rmd for SAFE
@@ -11,26 +11,25 @@
 #devtools::install_github("seacode/gmacs", subdir = "/gmr", ref = "develop") - only needs to be performed once.
 # alternate gmr instructions:::
 # if gmr is updated and above doesn't work try:
-# go to Build above - direct it to the gmr folder - press OK. Over on right in the Build tab (upper right hand side) - 
+# go to Build above (menu option in R studio) - "configure build tools" - direct it (package directory option) 
+#   to the gmr folder - press OK. 
+# Over on right hand size in the tabs (between connections and git should be build) in the Build tab (upper right hand side) - 
 # click "install and restart"
 require(gmr)
 source("./SMBKC/code/functions.R") 
 source("./SMBKC/code/helper.R") 
-source("./SMBKC/smbkc_19a/doc/gmr_functions2020.R") 
+source("./SMBKC/smbkc_20/doc/gmr_functions2020.R") 
 
 # ALL Model setup  -------------------------
 # first model is reference to previous year
-cur_yr <- 2019 # update annually 
-folder <- "smbkc_19a" # update annually 
+cur_yr <- 2020 # update annually 
+folder <- "smbkc_20" # update annually 
 
 # update model names and file locations
-mod_names <- c("model 16.0", "model 16.0 (ref)", "model 19.1 (VAST)", "model 19.2 (add CV pot)", "model 19.3 (add CV both)", "model 19.4 (q time block pot)") 
-.MODELDIR = c(paste0(here::here(), "/SMBKC/smbkc_19/model_1/"), 
-              paste0(here::here(), "/SMBKC/smbkc_19a/model_1/"),
-              paste0(here::here(), "/SMBKC/smbkc_19a/model_4/"), 
-              paste0(here::here(), "/SMBKC/smbkc_19a/model_1a/"), 
-              paste0(here::here(), "/SMBKC/smbkc_19a/model_1b/"),
-              paste0(here::here(), "/SMBKC/smbkc_19a/model_3/")) #need to update these model options
+mod_names <- c("model 16.0 (2019)", "model 16.0 (2020 base)", "model 20.1 (no pot)") 
+.MODELDIR = c(paste0(here::here(), "/SMBKC/smbkc_19a/model_1/"),
+              paste0(here::here(), "/SMBKC/smbkc_20/model_1/"), 
+              paste0(here::here(), "/SMBKC/smbkc_20/model_2/")) #need to update these model options
 .THEME    = theme_bw(base_size = 12, base_family = "")
 .OVERLAY  = TRUE
 .SEX      = c("Aggregate","Male")
@@ -39,7 +38,8 @@ mod_names <- c("model 16.0", "model 16.0 (ref)", "model 19.1 (VAST)", "model 19.
 .SHELL    = c("Aggregate")
 .MATURITY = c("Aggregate")
 .SEAS     = c("1","2","3","4","5")
-.FIGS     = c("./SMBKC/smbkc_19a/doc/safe_figure/")
+.FIGS     = c("./SMBKC/smbkc_20/doc/safe_figure/")
+.TABS     = c("./SMBKC/smbkc_20/doc/safe_tables/")
 # Read report file and create gmacs report object (a list):
 fn       <- paste0(.MODELDIR, "gmacs")
 M        <- lapply(fn, read_admb)
@@ -61,40 +61,33 @@ rinline <- function(code){
 }
 
 #alt_mod <- 5 # alt reference time frame
-ref_mod <- 1 # base
+ref_mod <- 1 # base 2019
 rec_mod <- 2 # base
-mod_scen<- 2:6 #scenarios you want graphed together
-mod_scen2 <- c(2, 6) # base model and q time block
-mod_scen3 <- 2:5 # without q time block
+mod_scen<- 2:3 #scenarios you want graphed together
 
 ww <- 6
 hh <- 5
 
 ## FIGURES ===================================
 ## data extent -----------
-plot_datarange(M[ref_mod])
+#plot_datarange(M[ref_mod]) # call in gmr not working - need to edit this on github
 plot_datarangeSM(M[rec_mod]) # see gmr_functions2020.R
+plot_datarangeSM(M[ref_mod]) # compare to 2019 base model 
 
+# save the rec_mod one
 # had to save manually because I can't get this call to work...**FIX
 
 ##!! fig 6/7 Last year's model compared to reference model --------
-# for final doc will be models 1 and 2, 1 will be updated to be smbkc_19a/model_1 
-#     and 2 will be smbkc_20/model_1 
-plot_cpue(M[2], "NMFS Trawl", ylab = "Survey biomass (t)")
-#plot_cpue(M[1:2], "NMFS Trawl", ylab = "Survey biomass (t)") 
+# models 1 and 2, 1 is 2019 model - smbkc_19a/model_1 
+#     and 2 is smbkc_20/model_1 base model
+plot_cpue(M[1:2], "NMFS Trawl", ylab = "Survey biomass (t)")
 ggsave(paste0(.FIGS, "trawl_cpue_ref.png"), width = ww*1.08, height = hh*.9)
 
-plot_cpue(M[2], "ADF&G Pot", ylab = "Pot survey CPUE (crab/potlift)")
-#plot_cpue(M[1:2], "ADF&G Pot", ylab = "Pot survey CPUE (crab/potlift)")
+plot_cpue(M[1:2], "ADF&G Pot", ylab = "Pot survey CPUE (crab/potlift)")
 ggsave(paste0(.FIGS, "pot_cpue_ref.png"), width = ww*1.08, height = hh)
 
-plot_cpue(M[2])
+plot_cpue(M[1:2])
 ggsave(paste0(.FIGS, "cpue_ref_both.png"), width = ww*2.5, height = hh)
-
-
-
-
-
 
 
 ### Sensitivity of new data in 2018 on estimated recruitment ; 1978-2018
