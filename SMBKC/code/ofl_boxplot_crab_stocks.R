d02f982e4d51fc47dc7a5c8e6098a014cf4f6efa
kjpalof <katie.palof@alaska.gov>
2020-09-21 12:26:31 -0800

    updates to all crab stocks boxplot to show raw samples

--- SMBKC/code/ofl_boxplot_crab_stocks.R
+++ SMBKC/code/ofl_boxplot_crab_stocks.R
@@ -19,3 +19,17 @@ ggplot(ofl7,aes(x=OFL,y=spp,fill=spp)) +
         legend.text=element_text(size=12),legend.position="none")
 
 ggsave(("ofl_boxplot_terminalyearsurvey.png"), width = 1.15*6, height = 5)
+
+ggplot(ofl7,aes(x=OFL,y=spp,fill=spp)) +
+  geom_boxplot(coef = 0) + 
+  geom_vline(xintercept=0) +
+  scale_fill_viridis(name="",discrete=TRUE,alpha=0.75) +
+  geom_point(color="black", size = 1.5, alpha=0.9) +
+  xlab("Proportional difference in OFL (no Survey - survey)/survey") +
+  theme_bw() +
+  theme(strip.text.x=element_text(size=12,color="black",face="bold.italic"),strip.background=element_rect(fill="white"))+
+  theme(axis.title.x = element_text(size = 12),axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12),axis.title.y = element_text(size = 12),
+        legend.text=element_text(size=12),legend.position="none")
+
+ggsave(("ofl_boxplot_terminalyearsurvey2.png"), width = 1.15*6, height = 5)
