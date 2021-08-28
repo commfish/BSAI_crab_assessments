964648261cbf3d1ae57cc057448204b1d1c9a0a1
kjpalof <katie.palof@alaska.gov>
2021-08-27 19:47:38 -0800

    updates to groundfish bycatch summary for 2021 off cycle year

--- SMBKC/code/bycatch_groundfish.R
+++ SMBKC/code/bycatch_groundfish.R
@@ -16,11 +16,12 @@
 #   
 # load -----
 source("./SMBKC/code/packages.R")
-model_yr = "smbkc_20"
+model_yr = "smbkc_21"
+cur_yr = 2021
 
 # data -----
 #gf_bycatch <- read.csv("C:/Users/kjpalof/Documents/SMBKC/DATA_SMBKC/Crab Bycatch Estimates.csv") # old location
-gf_bycatch <- read.csv(paste0(here::here(), '/SMBKC/smbkc_20/data/Crab Bycatch Estimates.csv'))
+gf_bycatch <- read.csv(paste0(here::here(), '/SMBKC/', model_yr, '/data/Crab Bycatch Estimates.csv'))
 
 # clean-up data ---------
 head(gf_bycatch)
@@ -41,8 +42,15 @@ gf_bycatch %>%
   summarise(sum = sum(Estimate.Wt..kg.crab.)) %>% 
   reshape2::dcast( ., Crab.Year ~ Agency.Gear.Code, sum) %>% 
   mutate(trawl = (NPT + PTR)/2, fixed = (HAL + POT)/2, 
-         trawl_thou = trawl/1000, fixed_thou = fixed/1000) -> gf_by_weight# combine trawl and fixed, divide by 2 - MALE only
+         trawl_thou = trawl/1000, fixed_thou = fixed/1000, 
+         trawl_1000t = round(trawl_thou/1000, 3), fixed_1000t = round(fixed_thou/1000, 3), 
+         status = ifelse(Crab.Year == cur_yr, "not completed", "complete"), 
+         total_1000t = trawl_1000t + fixed_1000t, 
+         total_lbs = round(total_1000t/0.453592, 3)) -> gf_by_weight
+# combine trawl and fixed, divide by 2 - MALE only
+# from SAFE intro  -1000 t to million lb [/0.453592]
 # ignore 2019, this is just the beginning of this season, data up until 2018
+
 write.csv(gf_by_weight, paste0(here::here(), '/SMBKC/', model_yr, '/data/gf_bycatch_weight.csv'), 
           row.names = FALSE)
 
