6bc1f15fe3ba4c3df878cca3b0aea701d1d21f2c
kjpalof <katie.palof@alaska.gov>
2022-04-14 20:59:56 -0800

    groundfish bycatch summary code

--- SMBKC/code/bycatch_groundfish.R
+++ SMBKC/code/bycatch_groundfish.R
@@ -1,5 +1,5 @@
 # k.palof katie.palof@alaska.gov
-# date updated: 8-12-19
+# date updated: 8-12-19 / 4-14-22
 
 # Data manipulation for bycatch in Groundfish fisheries for SMBKC
 
@@ -11,12 +11,12 @@
 #   From drop down menus "2009 to 2020", Fishery name: "Saint Matthew blue king crab", 
 #   Species Group Name: Blue King Crab
 #   Click - "detail report" 
-#    at bottom of page click "export", choose Data - .csv - save in data folder for current year - here smbkc_20/data
+#    at bottom of page click "export", choose Data - .csv - save in data folder for current year - here smbkc_XX/data
 
 #   
 # load -----
 source("./SMBKC/code/packages.R")
-model_yr = "smbkc_21"
+model_yr = "smbkc_22"
 cur_yr = 2021
 
 # data -----
@@ -29,10 +29,15 @@ head(gf_bycatch)
 # Agency.Gear.Code: HAL [hook & line], PTR [pelagic trawl], POT [pot], NPT [non-pelagic trawl]
 gf_bycatch %>% 
   group_by(Crab.Year, Agency.Gear.Code) %>% 
-  summarise(sum = sum(Estimate.Num)) %>% 
+  summarise(sum = sum(Estimate.Num..Sum.)) %>% # weird export
   reshape2::dcast( ., Crab.Year ~ Agency.Gear.Code, sum) %>% 
   mutate(trawl = (NPT + PTR)/2, fixed = (HAL + POT)/2) -> gf_by_nums# combine trawl and fixed, divide by 2 - MALE only
-# ignore 2019, this is just the beginning of this season, data up until 2018
+
+# this is for weight only  
+ #mutate(trawl_thou = trawl/1000, fixed_thou = fixed/1000) -> gf_by_nums# needs to be divided by 1,000 to be metric tons.
+# original is 1,000 kg with is 1ton, divide by 1,000 to get metric tons
+
+# ignore most recent year , this is just the beginning of this season, data up until current year -2 if May, current year - 1 if Sept.
 write.csv(gf_by_nums, paste0(here::here(), '/SMBKC/', model_yr, '/data/gf_bycatch_numbers.csv'), 
           row.names = FALSE)
 
