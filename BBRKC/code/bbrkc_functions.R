541f25023acbcd802e5c89ac6a50daf023d8631d
kjpalof <katie.palof@alaska.gov>
2023-04-30 14:55:02 -0800

    updates to functions specific to bbrkc

--- BBRKC/code/bbrkc_functions.R
+++ BBRKC/code/bbrkc_functions.R
@@ -53,6 +53,58 @@ bubbleplot_resid <- function(letter_m, sex, ltitle = "Model")
   print(p);
 }
 
+# jie's read function for GMACS output ---------------
+read_rep <- function(fn)
+{
+  options(warn = -1) # Suppress the NA message in the coercion to double
+  repfile <- scan(fn, what = "character", flush = TRUE, blank.lines.skip = FALSE, quiet = TRUE, na.strings = c("nan","-nan"))
+  #repfile <- scan(fn, what = "character", flush = TRUE, blank.lines.skip = FALSE, quiet = TRUE)
+  inan <- which(is.na(repfile)) # Identify any nan entries so that they are not picked up as objects
+  idx <- sapply(as.double(repfile), is.na)
+  idx[inan] <- FALSE
+  vnam <- repfile[idx] # list names
+  nv <- length(vnam) # number of objects
+  A <- list()
+  ir <- 0
+  for (i in 1:nv)
+  {
+    ir <- match(vnam[i], repfile)
+    if (i != nv)
+    {
+      irr <- match(vnam[i+1], repfile)
+    } else {
+      irr <- length(repfile) + 1 # next row
+    }
+    dum <- NA
+    if (irr-ir == 2)
+    {
+      dum <- as.double(scan(fn, skip = ir, nlines = 1, quiet = TRUE, what = ""))
+    }
+    if (irr-ir > 2)
+    {
+      # ncols <- 0
+      # irows <- ir:irr-1
+      # for(j in irows)
+      # {
+      #       tmp=as.double(scan(fn,skip=j,nlines=1,quiet=TRUE,what=""))
+      #       if(length(tmp)>ncols) ncols <- length(tmp)
+      #       #print(paste(1:ncols))
+      # }
+      # cname <- paste(1:ncols)
+      # dum=as.matrix(read.table(fn,skip=ir,nrow=irr-ir-1,fill=TRUE,col.names=cname))
+      # cat("\n ir ",ir," irr ",irr)
+      dum <- as.matrix(read.table(fn, skip = ir, nrow = irr-ir-1, fill = TRUE, row.names = NULL))
+    }
+    if (is.numeric(dum)) # Logical test to ensure dealing with numbers
+    {
+      A[[vnam[i]]] <- dum
+    }
+  }
+  options(warn = 0)
+  return(A)
+}
+
+
 
 # size comp plotting to include season as a divider ------------------
 plot_size_comps_kjp <- 
@@ -390,5 +442,28 @@ plot_selectivity_kjp <- function (M, subsetby = "", xlab = "Mid-point of size cl
                  panel.border = element_blank(), panel.background = element_blank(), 
                  strip.background = element_rect(color = "white", 
                                                  fill = "white"))
+  print(p)
+}
+
+# size composition residuals by fleet -------------------
+plot_size_comps_res_kjp <- function (M, subsetby = "", ncol = 1, xlab = "Year", ylab = "Mid-point of size-class (mm)") 
+{
+  seas <- NULL
+  xlab <- paste0(xlab, "\n")
+  ylab <- paste0(ylab, "\n")
+  mdf <- .get_sizeComps_df(M) %>% reshape2::melt(id = c("model", 
+                                                        "year", "seas", "fleet", "sex", 
+                                                        "type", "shell", "maturity", "nsamp", 
+                                                        "variable", "value", "pred", "resd")) %>% 
+    dplyr::mutate(seas = paste("Season", seas))
+  if (subsetby != "") mdf <- subset(mdf, fleet == subsetby)
+  
+  p <- ggplot(data = mdf) + geom_point(aes(factor(year), variable, 
+                                           col = factor(sign(resd)), size = abs(resd)), alpha = 0.6) + 
+    scale_size_area(max_size = 10) + labs(x = xlab, y = xlab, 
+                                          col = "Sign", size = "Residual") + facet_wrap(fleet ~ 
+                                                                                          seas, scales = "free_x", ncol = ncol) +
+                                              facet_wrap(sex ~ fleet, scales = "free_x", ncol = ncol) + .THEME + 
+    theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
   print(p)
 }
