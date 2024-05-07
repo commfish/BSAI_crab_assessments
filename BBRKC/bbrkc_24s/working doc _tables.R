7bec717b609c2d6a46093d19b7e56ef6e733f285
kjpalof <katie.palof@alaska.gov>
2024-05-06 22:12:42 -0800

    24s function /code that I haven't finalized yet for the figures tables R script

--- BBRKC/bbrkc_24s/working doc _tables.R
+++ BBRKC/bbrkc_24s/working doc _tables.R
@@ -85,7 +85,7 @@ temp3 %>%
   summarise(total_obs = round(sum(obs_index)/1000, 2), # total area swept
             total_pred = round(sum(pred_index)/1000, 2)) -> survey_est # total model est survey
 
-# put them in the correct order for tables in doc -------
+# put them in the correct order for tables in doc -
 #abun_tab <- cbind(fmales1[ ,1:3], derived_m[ ,2:3], recruit_tab, survey_est[ ,c(3,2)])
 survey_est %>% #[1:47, ] %>% 
   merge(derived_m, all = T) %>% 
@@ -197,3 +197,131 @@ gmacs_plot_index <- function(all_out = NULL, save_plot = T, plot_dir = NULL, y_l
   } else {return(plots$plot)}
   
 }
+
+
+
+
+# Selectivity fix for females ----------------------------------
+gmacs_get_slx(all_out = base_models) %>%
+  mutate(capture_block = case_when(fleet %in% c("BSFRF", "Bairdi_Fishery_Bycatch", "Fixed_Gear") ~ "1975 - 2023",
+                                   fleet == "NMFS_Trawl" & year %in% 1975:1981 ~ "1975 - 1981",
+                                   fleet == "NMFS_Trawl" & year %in% 1981:2023 ~ "1981 - 2023",
+                                   fleet == "Pot_Fishery" ~ "1975 - 2022",
+                                   fleet == "Trawl_Bycatch" ~ "1975 - 2022")) %>%
+  gmacs_plot_slx(data_summary = ., save_plot = T, )
+
+
+
+# NEED TO TRY THOSE BELOW ----------------
+# From Caitlin for MMB with projected year ----------------------------------------
+# modified version of gmacs_get_ref_points function that brings in SE on SSB
+gmacs_get_ref_points <- function(all_out = NULL, file = NULL, model_name = NULL){
+  # bring in all out data ----
+  if(!is.null(file) && is.null(all_out)) {
+    if(is.null(model_name)) {stop("Must supply model name(s)!!")}
+    # read all out file
+    all_out <- purrr::map2(file, model_name, gmacs_read_allout); names(all_out) <- paste0("model", model_name)
+  }
+  
+  # extract catch data ----
+  tibble(mod = names(all_out),
+         all_out = all_out) %>%
+    mutate(data = purrr::map(all_out, function(x) {
+      x$reference_points %>%
+        transmute(parameter_name, estimate, se) %>%
+        pivot_wider(names_from = parameter_name, values_from = c(estimate, se)) %>%
+        transmute(model = as.character(x$model_name),
+                  mmb = estimate_BMSY * `estimate_Bcurr/BMSY`,
+                  b35 = estimate_BMSY,
+                  b_b35 = `estimate_Bcurr/BMSY`,
+                  male_rbar = `estimate_Male_spr_rbar`,
+                  rbar_yrs = paste(rep24.0$spr_syr, "-", rep24.0$spr_nyr),
+                  f35 = `estimate_Fmsy_1`,
+                  fofl = rep24.0$spr_fofl * f35,
+                  ofl_tot = estimate_OFL_tot,
+                  b35_se = se_BMSY,
+                  b_b35_se = `se_Bcurr/BMSY`,
+                  mmb_se = (`estimate_Bcurr/BMSY` * se_BMSY) + (estimate_BMSY * `se_Bcurr/BMSY`))
+    })) %>% transmute(data) %>% unnest(data) -> out
+  return(out)
+}
+
+
+
+# projections for models 16.0, 16.0a, 16.0b, and 24.0
+stds.16.24 <- bind_rows(gmacs_read_std("./SMBKC/smbkc_24s/model_16_0/gmacs.std", sub_text = "sd_log_ssb", model_name = "16.0") %>% mutate(year = 1978:2022),
+                        gmacs_read_std("./SMBKC/smbkc_24s/model_16_0_a/gmacs.std", sub_text = "sd_log_ssb", model_name = "16.0a") %>% mutate(year = 1978:2022),
+                        gmacs_read_std("./SMBKC/smbkc_24s/model_16_0_b/gmacs.std", sub_text = "sd_log_ssb", model_name = "16.0b") %>% mutate(year = 1978:2022),
+                        gmacs_read_std("./SMBKC/smbkc_24s/model_24_0/gmacs.std", sub_text = "sd_log_ssb", model_name = "24.0") %>% mutate(year = 1978:2022)) %>%
+  mutate(ssb_se = se / (1 / exp(est)), 
+         ssb_lci = exp(est) + ssb_se * qnorm(0.05 / 2), 
+         ssb_uci = exp(est) + ssb_se * qnorm(1 - 0.05 / 2))
+
+refs.16.24 <- gmacs_get_ref_points(all_out = list(rep16.0, rep16.0a, rep16.0b, rep24.0)) %>%
+  rename(model_name = model) %>%
+  mutate(ssb = mmb,
+         ssb_se = mmb_se, 
+         ssb_lci = mmb + ssb_se * qnorm(0.05 / 2), 
+         ssb_uci = mmb + ssb_se * qnorm(1 - 0.05 / 2),
+         year = 2023)
+
+proj.plot.1 <- gmacs_get_derived_quantity_summary(all_out = list(rep16.0, rep16.0a, rep16.0b, rep24.0)) %>%
+  rename(model_name = model) %>%
+  left_join(stds.16.24) %>%
+  bind_rows(refs.16.24)
+
+plot1 <- proj.plot.1 %>%
+  ggplot()+
+  geom_ribbon(aes(x = as.numeric(as.factor(year)), ymin = ssb_lci, ymax = ssb_uci, fill = model_name), alpha = 0.2, show.legend = F)+
+  geom_line(aes(x = factor(year), y = ssb, group = model_name, color = model_name))+
+  geom_point(data = function(x) filter(x, year == max(year)),
+             aes(x = factor(year), y = ssb, color = model_name))+
+  scale_x_discrete(breaks = yraxis$breaks, labels = yraxis$labels)+
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA))+
+  scale_color_manual(values = cbpalette)+
+  scale_fill_manual(values = cbpalette)+
+  labs(x = NULL, y = paste0("MMB (t)"), color = NULL)+
+  theme(legend.position = c(1, 1),
+        legend.justification = c(1, 1))
+
+ggsave("./SMBKC/smbkc_24s/model_24_0/plots/mmb_trajectory_modelset1.png", plot1, height = 4.2, width = 7, units = "in")
+
+# Tyler plots with projected SSB -------------------------------
+gmacs_get_derived_quantity_summary(eag_ao) %>%
+  left_join(gmacs_get_ref_points(eag_ao)) %>%
+  left_join(gmacs_read_std("AIGKC/models/2024/may/EAG/23.1/gmacs.std", sub_text = "sd_log_ssb", model_name = "23.1") %>%
+              transmute(model = "23.1", year = 1960:2023,
+                        ssb_se = se / (1 / exp(est)), 
+                        ssb_lci = exp(est) + ssb_se * qnorm(0.05 / 2), 
+                        ssb_uci = exp(est) + ssb_se * qnorm(1 - 0.05 / 2)), by = c("model", "year")) %>%
+  ggplot()+
+  geom_ribbon(aes(x = as.numeric(as.factor(year)), ymin = ssb_lci, ymax = ssb_uci, fill = model), alpha = 0.2, show.legend = F)+
+  geom_line(aes(x = factor(year), y = ssb, group = model, color = model))+
+  geom_point(data = function(x) filter(x, year == max(year)),
+             aes(x = factor(year), y = mmb, color = model))+
+  scale_x_discrete(breaks = yraxis$breaks, labels = yraxis$labels)+
+  scale_y_continuous(labels = scales::comma, limits = c(0, NA))+
+  scale_color_manual(values = cbpalette)+
+  scale_fill_manual(values = cbpalette)+
+  labs(x = NULL, y = paste0("MMB (t)"), color = NULL)+
+  theme(legend.position = c(1, 1),
+        legend.justification = c(1, 1))
+
+
+### plotting all F's on one plot from Caitlin -----------------------------
+
+p1 <- gmacs_plot_f(all_out = list(rep24.0), save_plot = F, yrs = 1978:2022)[[1]]
+p2 <- gmacs_plot_f(all_out = list(rep24.0), save_plot = F, yrs = 1978:2022)[[2]]
+p3 <- gmacs_plot_f(all_out = list(rep24.0), save_plot = F, yrs = 1978:2022)[[3]]
+p4 <- gmacs_plot_f(all_out = list(rep24.0), save_plot = F, yrs = 1978:2022)[[4]]
+p5 <- gmacs_plot_f(all_out = list(rep24.0), save_plot = F, yrs = 1978:2022)[[5]]
+
+
+f_comb <- plot_grid(
+  p2, p4, p5,
+  labels = "", ncol = 1
+)
+
+yrs <- 1978:2022
+pwidth <- min(max(length(min(yrs):max(yrs))*0.2, 5), 7)
+ggsave(file.path("./SMBKC/smbkc_24s/model_24_0/plots", "f_combined.png"), plot = f_comb, height = 0.6*pwidth, width = pwidth, units = "in")
